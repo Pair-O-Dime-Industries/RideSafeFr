c40026f536322ba33a00e2d2a59185fcc0c9fa78
Egan Hernandez <egan@cenedex.com>
2021-04-18 16:15:36 -0400

    2nd merge

--- actions/script.js
+++ actions/script.js
@@ -14,34 +14,33 @@ function clickme() {
         <style>
     } `;
 };
-logout.onclick = async(e) =>{
-    console.log('worked');
-     e.preventDefault();
-     let data = {logout:'logout'};
-     var oReq = new XMLHttpRequest(); 
+// logout.onclick = async(e) =>{
+//     console.log('worked');
+//      e.preventDefault();
+//      let data = {logout:'logout'};
+//      var oReq = new XMLHttpRequest(); 
   
-      oReq.open("POST", "../backend/login.php", true);
-          oReq.setRequestHeader("Content-Type", "application/json");
-        oReq.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-        var response = this.responseText;
+//       oReq.open("POST", "../backend/login.php", true);
+//           oReq.setRequestHeader("Content-Type", "application/json");
+//         oReq.onreadystatechange = function(){
+//         if (this.readyState == 4 && this.status == 200){
+//         var response = this.responseText;
 
-        if(response == true){
-            window.location.href="./login.html";
+//         if(response == true){
+//             window.location.href="./login.html";
         
-        }
+//         }
 
         
-        }
-        };
-        oReq.send(JSON.stringify(data));
-}
+//         }
+//         };
+//         oReq.send(JSON.stringify(data));
+// }
 function check() { //Rider turns white and Driver turns yellow. Next button is yellow
     var rider = document.getElementById("Rider");
     var driver = document.getElementById("Driver");
     var button = document.getElementById("next-btn").style;
-    var l = document.getElementById("signup-hide").style;
-    var i = document.getElementById("signup-show").style;
+    
 
     rider.style.backgroundColor = "white";
     rider.style.color = "black";
@@ -61,8 +60,7 @@ function uncheck() {
     var rider = document.getElementById("Rider");
     var driver = document.getElementById("Driver");
     var button = document.getElementById("next-btn").style;
-    var l = document.getElementById("signup-hide").style;
-    var i = document.getElementById("signup-show").style;
+ 
 
     rider.style.backgroundColor = "rgb(35, 10, 124)";
     rider.style.color = "white";
