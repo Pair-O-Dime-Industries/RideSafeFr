1fd6fc6e1b09d28aa7fbf78dddb5619cdee9b704
Egan Hernandez <egan@cenedex.com>
2021-04-23 22:09:06 -0400

    changed scanner to read from back camera

--- actions/script.js
+++ actions/script.js
@@ -227,8 +227,9 @@ async function scan(){
 
     Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
-    scanner.start(cameras[0]);
+    scanner.start(cameras[1]);
     } else {
+        
     console.error('No cameras found.');
     }
     }).catch(function (e) {
